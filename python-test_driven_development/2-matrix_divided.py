a29ee975c20425242a4de55bf1847968dff42dcd
Christia3 <ntwaliishimwe@gmail.com>
2024-11-27 09:30:56 +0000

    hello

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
 """
-This module supplies one function.
+This is a Matrix Divided function
 """
 
 
-def add_integer(a, b):
-    """The addition of 2 numbers."""
-    if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-    return a + b
+def matrix_divided(matrix, div):
+    """Divides all elements in a matrix"""
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix of integers/floats")
+    size = None
+    for yy in matrix:
+        if type(yy) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        if size is None:
+            size = len(yy)
+        elif size != len(yy):
+            raise TypeError("Each row of the matrix must have the same size")
+        for i in yy:
+            if type(i) is not int and type(i) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [[round(i / div, 2) for i in yy] for yy in matrix]
